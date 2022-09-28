147359cf545270888cdf08d00a7df652d73e722a
tyxo0 <87252115+tyxo0@users.noreply.github.com>
2022-09-28 23:26:09 +0900

    Delete PROJECT220921_박태영 directory

--- PROJECT220921_박태영/DoWhileSample.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileSample { 
-    public static void main (String[] args) { 
-        char c = 'a';
-        do { 
-            System.out.print(c); 
-            c = (char) (c + 1); 
-        } while (c <= 'z');
-    }
-}
